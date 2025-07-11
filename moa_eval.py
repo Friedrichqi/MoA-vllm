c67bff53416c088c1c5e2cce7fc9f6d1ec32cd3e
Friedrichqi <2169018621@qq.com>
2025-07-12 02:26:57 +0800

    fix few_shots=0 bug

--- moa_eval.py
+++ moa_eval.py
@@ -153,12 +153,13 @@ def main(args):
         return
 
     # Fetching few-shot learning samples
-    with open(os.path.join("benchmark", f"{args.benchmark}_train.jsonl")) as f:
-        few_shots = []
-        for id, line in enumerate(f.readlines()):
-            if id >= args.few_shots:
-                break
-            few_shots.append(json.loads(line))
+    if args.few_shots > 0:
+        with open(os.path.join("benchmark", f"{args.benchmark}_train.jsonl")) as f:
+            few_shots = []
+            for id, line in enumerate(f.readlines()):
+                if id >= args.few_shots:
+                    break
+                few_shots.append(json.loads(line))
     
     # Prepare arguments for multiprocessing
     args_list = [(entry, args, few_shots, configs) for entry in entries_to_process]
